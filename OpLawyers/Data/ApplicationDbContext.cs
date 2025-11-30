4ec53f151b35975e253e1b6e59cca476e73f9d8d
FelixAlbertoM <af301509@gmail.com>
2025-11-30 15:06:07 -0400

    borrando migracion predeterminado

--- OpLawyers/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace OpLawyers.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
